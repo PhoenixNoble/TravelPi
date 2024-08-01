5bd12551a9aa8c5a1c77d8694d76af932c9b8a21
PhoenixNoble <129368967+PhoenixNoble@users.noreply.github.com>
2024-08-01 10:14:17 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,9 @@
-from flask import Flask,request
+from flask import Flask,request, render_template_string
 import subprocess
 
 app = Flask(__name__)
 
-wifi_device = "wlan1"
+wifi_device = "wlan0"
 
 def GetAvailableNetworks():
     html = ""
@@ -20,26 +20,58 @@ def GetAvailableNetworks():
         for ssid in ssids_list:
             only_ssid = ssid.removeprefix("SSID:")
             if len(only_ssid) > 0:
-                dropdowndisplay += f"""
+                html += f"""
                 <option value="{only_ssid}">{only_ssid}</option>
                 """
+        # <input type="password" name="password"/>
         html += f"""
                 </select>
                 <p/>
-                <label for="password">Password: <input type="password" name="password"/></label>
+                <label for="password">Password: <input name="password"/></label>
                 <p/>
                 <input type="submit" value="Connect">
             </form>
             """
-            
+
     except subprocess.CalledProcessError as e:
         html += f"""
             <h1>No Wifi Device Found</h1>
             """
 
     return html
-    
 
+@app.route('/data_temperature')
+def data():
+    temp = subprocess.check_output(["vcgencmd", "measure_temp"])
+    temp = temp.decode().strip('\n')
+    return temp
+
+@app.route('/pistats')
+def pistats(): 
+    arm_mem = subprocess.check_output(["vcgencmd", "get_mem", "arm"])
+    arm_mem = arm_mem.decode().strip('\n')
+    gpu_mem = subprocess.check_output(["vcgencmd", "get_mem", "gpu"])
+    gpu_mem = gpu_mem.decode().strip('\n')
+
+    html = f"""
+    {arm_mem}
+    <p/>
+    {gpu_mem}
+    <p/>
+    """
+    html += render_template_string("""
+    <span id="time"><span>
+    <script type="text/javascript">
+    var time_span = document.getElementById("time");
+    function updater() {
+    fetch('/data_temperature')
+    .then(response => response.text())
+    .then(text => (time_span.innerHTML = text));  // update page with new data
+    }
+    setInterval(updater, 250);  // run `updater()` every 1000ms (1s)
+    </script>
+    """)
+    return html
 
 @app.route('/')
 def index():
@@ -47,50 +79,55 @@ def index():
         <!DOCTYPE html>
         <html>
         <head>
+            <meta charset="utf-8" />
             <title>Wifi Control</title>
         </head>
         <body>
         """
     dropdowndisplay += GetAvailableNetworks()
-    
     dropdowndisplay += f"""
             <form action="/changebssid" method="post">
                 <p/>
-                <label for="bssid">New BSSID: <input type="password" name="bssid"/></label>
+                <label for="bssid">New BSSID: <input name="bssid"/></label>
                 <p/>
                 <input type="submit" value="Set BSSID">
                 <p/>
             </form>
             <form action="/shutdown" method="post">
-                <h1>Jellyfin 8096</h1>
                 <input type="submit" value="Shutdown">
             </form>
             <form action="/reboot" method="post">
                 <p/>
                 <input type="submit" value="Reboot">
             </form>
+            <p/>
+            <a href="http://travelpi.local:8096">Jellyfin 8096</a>
+            <p/>
+            <a href="http://travelpi.local/pistats">Stats</a>
         </body>
-            </html>
-            """
+    </html>
+    """
     return dropdowndisplay
 
 @app.route('/shutdown',methods=['POST'])
 def shutdown():
     if request.method == 'POST':
         result = subprocess.run(["sudo", "shutdown", "now"], capture_output=False)
-    return "Nopte Buna!"
+    return "Shutdown!"
 
 @app.route('/reboot',methods=['POST'])
 def reboot():
     if request.method == 'POST':
         result = subprocess.run(["sudo", "reboot"], capture_output=False)
-    return "Nopte Buna!"
+    return "Reboot!"
 
 @app.route('/changebssid',methods=['POST'])
 def changebssid():
     if request.method == 'POST':
         bssid = request.form['bssid']
-        result = subprocess.run(["sudo", "nmcli", "connection", "modify", "9482dc26-d9e9-40df-b1bd-b3595b63463b", "802-11-wireless.ssid", bssid], capture_output=True)
+        #16f99539-6b26-48eb-9466-709854cb5dd4
+        #9482dc26-d9e9-40df-b1bd-b3595b63463b
+        result = subprocess.run(["sudo", "nmcli", "connection", "modify", "9b2eb23f-874b-46cb-8348-c6330dc4ec49", "802-11-wireless.ssid", bssid], capture_output=True)
         if result.stderr:
             return "Error: failed to connect to rename wifi network: <i>%s</i>" % result.stderr.decode()
         elif result.stdout:
